2ecbd8fc7a9d423f2f775e4447e767dc42a70fa9
gulan <glen.wilder@gmail.com>
2015-11-10 07:24:35 -0800

    Barrier synchronization of processes with pipes

--- python2/m_multiprocessing.py
+++ python2/m_multiprocessing.py
@@ -3,6 +3,7 @@
 import multiprocessing as MP
 import os
 import sys
+import time
 
 # These first tests are cribbed from the python.org docs.
 
@@ -142,6 +143,150 @@ def test_process_10():
     assert val == [0, 1, 2, 0, 1, 2, 0, 1, 2, 0]
     print 'test_process_10 ok'
 
+def test_process_11():
+    """
+    Barrier synchronization of two processes with a pipe.
+    """
+    
+    def process_1(pipe):
+        print "p1 1"
+        print "p1 1"
+        time.sleep(0.01)
+        print "p1 1"
+        pipe.send(''); pipe.recv() # 2
+        print "p1 2"
+        pipe.send(''); pipe.recv() # 3
+        print "p1 3"
+        time.sleep(0.02)
+        print "p1 3"
+        pipe.send(''); pipe.recv() # 4
+        pipe.send(''); pipe.recv() # 5
+        print "p1 5"
+        time.sleep(0.03)
+        print "p1 5"
+        print "p1 5"
+        pipe.close()
+
+    def process_2(pipe):
+        pipe.recv(); pipe.send('') # 2
+        print "p2 2"
+        time.sleep(0.04)
+        print "p2 2"
+        print "p2 2"
+        pipe.recv(); pipe.send('') # 3
+        time.sleep(0.02)
+        print "p2 3"
+        print "p2 3"
+        pipe.recv(); pipe.send('') # 4
+        print "p2 4"
+        time.sleep(0.01)
+        print "p2 4"
+        pipe.recv(); pipe.send('') # 5
+        print "p2 5"
+        print "p2 5"
+        pipe.close()
+        
+    p1, p2 = MP.Pipe()
+    pr1 = MP.Process(target=process_1, args=(p1,))
+    pr1.start()
+    pr2 = MP.Process(target=process_2, args=(p2,))
+    pr2.start()
+    
+    pr1.join()
+    pr2.join()
+    print 'test_process_11 ok'
+
+def test_process_12(): # FAIL
+    
+    """ raise EOFError on closed pipe """
+    
+    def f(pipe):
+        pipe.send('arbitrary value')
+        pipe.close()
+        time.sleep(1) # No help
+        
+    parent_end, child_end = MP.Pipe(duplex=False)
+    p = MP.Process(target=f, args=(child_end,))
+    p.start()
+    assert parent_end.recv() == 'arbitrary value'
+    print >>sys.stderr, 'checkpoint 1'
+    try:
+        parent_end.recv()  # Hangs
+    except EOFError:
+        pass
+    print >>sys.stderr, 'checkpoint 2'
+    p.join()
+    print 'test_process_12 ok'
+
+def test_process_13(N=3):
+    """
+    Barrier synchronization of N processes with a pipes and a helper.
+    """
+    def worker(i,pipe):
+        sp = 0
+        sp_ = pipe.recv()
+        assert sp_ == sp, "fail %s %s" % (i,sp)
+        print "%2.2d working after syncpoint %2.2d" % (i,sp)
+        
+        sp = 1
+        pipe.send(sp)
+        sp_ = pipe.recv()
+        assert sp_ == sp, "fail %s %s" % (i,sp)
+        print "%2.2d working after syncpoint %2.2d" % (i,sp)
+        
+        sp = 2
+        pipe.send(sp)
+        sp_ = pipe.recv()
+        assert sp_ == sp, "fail %s %s" % (i,sp)
+        print "%2.2d QUITING after syncpoint %2.2d" % (i,sp)
+        
+        pipe.close()
+        
+    def helper(plist):
+        print 'checkpoint helper 0'
+        sp = 0
+        for pi in plist: pi.send(sp)
+        print 'checkpoint helper 1'
+        
+        sp = 1
+        for i,pi in enumerate(plist):
+            assert pi.recv() == sp, 'fail sync %s' % i
+        print 'checkpoint helper 2'
+        for pi in plist:
+            pi.send(sp)
+        
+        sp = 2
+        for i,pi in enumerate(plist):
+            assert pi.recv() == sp, 'fail sync %s' % i
+        print 'checkpoint helper 3'
+        for pi in plist:
+            pi.send(sp)
+        
+        print 'checkpoint helper 4'
+        for pi in plist:
+            pi.close()
+            
+        print 'checkpoint helper 5 (end)'
+
+    worker_list, helper_list = [], []
+    for i in range(1, N+1):
+        w, h = MP.Pipe()
+        worker_list.append((i,w)) # pipes given to workers, 1 each
+        helper_list.append(h) # pipe list for helper
+    he = MP.Process(target=helper, args=(helper_list,))
+    he.start()
+    workers = []
+    for pi in worker_list:
+        wo = MP.Process(target=worker, args=(pi[0],pi[1]))
+        wo.start()
+        workers.append(wo)
+    print 'checkpoint main 1'
+    for wo in workers:
+        wo.join()
+    print 'checkpoint main 2'
+    he.join()
+    print 'test_process_13 ok'
+
 if __name__ == '__main__':
     test_process_01()
     test_process_02()
@@ -153,4 +298,7 @@ if __name__ == '__main__':
     test_process_08()
     test_process_09()
     test_process_10()
+    test_process_11()
+    # 12 fails
+    test_process_13()
 
