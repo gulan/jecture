856bef9d2223ae539a903a5cb425101a4ef4cf47
gulan <glen.wilder@gmail.com>
2015-11-11 23:48:42 -0800

    Add first Popen() case.

--- python3/m_subprocess.py
+++ python3/m_subprocess.py
@@ -8,6 +8,7 @@ import types
 r = None
 
 def subprocess_00():
+    # Examine subprocess module
     assert isinstance(subprocess.getoutput, types.FunctionType)
     assert isinstance(subprocess.getstatusoutput, types.FunctionType)
     assert isinstance(subprocess.check_output, types.FunctionType)
@@ -26,7 +27,7 @@ def subprocess_00():
     print ('subprocess_00 ok')
 
 def subprocess_01():
-    # simple run()
+    # Simple run()
     r = subprocess.run(['echo', 'a'])
     assert isinstance(r, subprocess.CompletedProcess)
     assert r.args == ['echo', 'a']
@@ -37,7 +38,7 @@ def subprocess_01():
     print ('subprocess_01 ok')
 
 def subprocess_02():
-    # error: 'echo b' is not the name of a program
+    # Error: 'echo b' is not the name of a program
     try:
         subprocess.run(['echo b'])
     except FileNotFoundError:
@@ -45,7 +46,7 @@ def subprocess_02():
     else:
         assert(False)
     
-    # ok: 'echo c' *is* valid shell input.
+    # OK: 'echo c' *is* valid shell input.
     r = subprocess.run(['echo c'], shell=True)
     assert isinstance(r, subprocess.CompletedProcess)
     assert r.args == ['echo c']
@@ -56,43 +57,100 @@ def subprocess_02():
     print ('subprocess_02 ok')
 
 def subprocess_03():
-    # capture stdout 1
+    # Capture stdout 1
     r = subprocess.run(['echo', 'd'], stdout=subprocess.PIPE)
     assert isinstance(r.stdout, type(b'a byte string'))
     assert r.stdout == b'd\n'
     
-    # capture stdout 2
+    # Capture stdout 2
     r = subprocess.run(['echo e'], shell=True, stdout=subprocess.PIPE)
     assert isinstance(r.stdout, type(b'a byte string'))
     assert r.stdout == b'e\n'
     
-    # no stdout to capture
+    # No stdout to capture
     r = subprocess.run(['echo f >/dev/null'], shell=True, stdout=subprocess.PIPE)
     assert isinstance(r.stdout, type(b'a byte string'))
     assert r.stdout == b''
     
-    # capture stderr
+    # Capture stderr
     r = subprocess.run(['echo g >/dev/stderr'], shell=True, stderr=subprocess.PIPE)
     assert isinstance(r.stderr, type(b'a byte string'))
     assert r.stderr == b'g\n'
     
-    # no stderr to capture
+    # No stderr to capture
     r = subprocess.run(['(echo hhh >/dev/stderr) 2>/dev/null'],
                        shell=True, stderr=subprocess.PIPE)
     assert isinstance(r.stderr, type(b'a byte string'))
     assert r.stderr == b''
     
     print ('subprocess_03 ok')
+
+def subprocess_04():
+    # arglist and shell == True
+    print ('subprocess_04 ok')
     
-if r:
-    print (r.args)
-    print (r.check_returncode)
-    print (r.returncode)
-    print (r.stderr)
-    print (r.stdout)
+def subprocess_05():
+    # How to use check_returncode()?
+    print ('subprocess_05 ok')
+
+def subprocess_10():
+    # Popen arguments
+    args = 'echo'
+    p = subprocess.Popen(
+        args,                    # sequence or string
+        bufsize=-1,              # io.DEFAULT_BUFFER_SIZE
+        # bufsize=0,             # unbuffered
+        # bufsize=1,             # line buffered
+        # bufsize=512,           # >1, approx. bufsize
+        executable=None,            # use program implied by args and shell
+        # executable=/usr/bin/bash  # if shell == True, use this instead of /bin/sh
+        # executable=xxx            # if shell == False, run xxx but display args[0] in ps
+        stdin=None,              # None | PIPE | DEVNULL | > 0 | file-object
+        stdout=None,             # None | PIPE | DEVNULL | > 0 | file-object
+        stderr=None,             # None | PIPE | STDOUT | DEVNULL | > 0 | file-object
+        preexec_fn=None,         # Call before running the child
+        close_fds=True,          # file descriptors > 2 before running child
+        shell=False,             # bool. See run()
+        cwd=None,
+        env=None,
+        universal_newlines=False,
+        startupinfo=None,
+        creationflags=0,
+        restore_signals=True,
+        start_new_session=False,
+        pass_fds=()
+    )
+    assert isinstance(p.communicate, types.MethodType)
+    assert isinstance(p.kill, types.MethodType)
+    assert isinstance(p.send_signal, types.MethodType)
+    assert isinstance(p.terminate, types.MethodType)
+    assert isinstance(p.wait, types.MethodType)
+    assert isinstance(p.poll, types.MethodType)
+
+    assert p.args == args
+    save_pid = p.pid
+    assert p.returncode == None
+    assert p.stdin == None
+    assert p.stdout == None
+    assert p.stderr == None
+    assert p.universal_newlines == False
+    
+    stdout_data, stderr_data = p.communicate(input=None, timeout=None)
+
+    assert p.args == args
+    assert p.pid == save_pid
+    assert p.returncode == 0
+    assert p.stdin == None
+    assert p.stdout == None
+    assert p.stderr == None
+    assert p.universal_newlines == False
+    print ('subprocess_10 ok')
 
 if __name__ == '__main__':
     subprocess_00()
     subprocess_01()
     subprocess_02()
     subprocess_03()
+    subprocess_04()
+    subprocess_05()
+    subprocess_10()
