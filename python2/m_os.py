b8f271b7d6ad0360db218d724a6cd75045d36b5a
gulan <glen.wilder@gmail.com>
2015-11-13 20:34:37 -0800

    More Python os module tests

--- python2/m_os.py
+++ python2/m_os.py
@@ -1,34 +1,12 @@
 #!/usr/bin/env python
 
 import os
+import pwd
+import signal
+import socket
 import sys
+import time
 import types
-
-def gen():
-    for i in dir(os):
-        if i[0] == '_':
-            continue
-        t = repr(type(os.__dict__[i]))
-        if t == "<type 'module'>":
-            continue
-        elif t == "<type 'builtin_function_or_method'>":
-            print '    assert isinstance(os.%s, types.BuiltinMethodType)' % i
-        elif t == "<type 'function'>":
-            print '    assert isinstance(os.%s, types.FunctionType)' % i
-        elif t == "<type 'instance'>":
-            print '    assert isinstance(os.%s, types.InstanceType)' % i
-        elif t == "<type 'type'>":
-            print '    assert isinstance(os.%s, type)' % i
-        elif t == "<type 'dict'>":
-            print '    assert isinstance(os.%s, dict)' % i
-        elif t == "<type 'NoneType'>":
-            print '    print os.%s' % i
-        elif t == "<type 'str'>":
-            print '    print os.%s' % i
-        elif t == "<type 'int'>":
-            print '    print os.%s' % i
-        else:
-            print 'X', t
 
 def os_01():
     assert os.EX_CANTCREAT == 73
@@ -249,15 +227,364 @@ def os_01():
     print 'os_01 ok'
 
 def os_02():
-    rc = os.fork()
-    if rc == 0:
-        print 'child %s' % os.getpid()
+    # TBD: write pid to file, confirm.
+    pid = os.fork()
+    if pid == 0:
+        # print 'child %s' % os.getpid()
         os._exit(0)
     else:
-        print 'parent %s' % os.getpid()
+        # print 'parent %s' % os.getpid()
+        pass
     print 'os_02 ok'
+
+def os_03():
+    chpid = os.fork()
+    if chpid == 0:
+        os._exit(0)
+    else:
+        try:
+            (pid, status) = os.waitpid(chpid,0)
+        except OSError, e:
+            print >>sys.stderr, 'unexpected error on wait()'
+            raise
+        # pid < -1   some child of process group abs(pip)
+        # pid == -1  some child of mine
+        # pid == 0   some child in my process group
+        # pid > 1    my child
+        assert pid > 0
+        assert os.WCOREDUMP(status) == False
+        assert os.WIFCONTINUED(status) == False
+        assert os.WIFSTOPPED(status) == False
+        assert os.WIFSIGNALED(status) == False
+        assert os.WIFEXITED(status) == True
+        assert os.WEXITSTATUS(status) == 0
+    print 'os_03 ok'
+
+def os_04():
+    chpid = os.fork()
+    if chpid == 0:
+        os._exit(1)
+    else:
+        (pid, status) = os.waitpid(chpid,0)
+        assert pid > 0
+        assert os.WCOREDUMP(status) == False
+        assert os.WIFCONTINUED(status) == False
+        assert os.WIFSTOPPED(status) == False
+        assert os.WIFSIGNALED(status) == False
+        assert os.WIFEXITED(status) == True
+        assert os.WEXITSTATUS(status) == 1
+    print 'os_04 ok'
+
+def os_05():
+    chpid = os.fork()
+    if chpid == 0:
+        time.sleep(0)
+        os._exit(0)
+    else:
+        (pid, status) = os.waitpid(chpid,0)
+        assert pid > 0
+        assert os.WCOREDUMP(status) == False
+        assert os.WIFCONTINUED(status) == False
+        assert os.WIFSTOPPED(status) == False
+        assert os.WIFSIGNALED(status) == False
+        assert os.WIFEXITED(status) == True
+        assert os.WEXITSTATUS(status) == 0
+    print 'os_05 ok'
+
+def os_06():
+    chpid = os.fork()
+    if chpid == 0:
+        time.sleep(1)
+        os._exit(0)
+    else:
+        os.kill(chpid, signal.SIGTERM)
+        (pid, status) = os.waitpid(chpid,0)
+        assert pid > 0
+        assert os.WCOREDUMP(status) == False
+        assert os.WIFCONTINUED(status) == False
+        assert os.WIFSTOPPED(status) == False
+        assert os.WIFSIGNALED(status) == True
+        assert os.WIFEXITED(status) == False
+        assert os.WEXITSTATUS(status) == 0
+        # print os.WSTOPSIG(status)
+        assert os.WTERMSIG(status) == signal.SIGTERM
+    print 'os_06 ok'
+
+def os_07():
+    chpid = os.fork()
+    if chpid == 0:
+        time.sleep(0.1)
+        os._exit(0)
+    else:
+        os.kill(chpid, signal.SIGSTOP)
+        os.kill(chpid, signal.SIGCONT)
+        (pid, status) = os.waitpid(chpid,0)
+        assert pid > 0
+        assert os.WCOREDUMP(status) == False
+        assert os.WIFCONTINUED(status) == False
+        assert os.WIFSTOPPED(status) == False
+        assert os.WIFSIGNALED(status) == False
+        assert os.WIFEXITED(status) == True
+        assert os.WEXITSTATUS(status) == 0
+        # print os.WSTOPSIG(status)
+        assert os.WTERMSIG(status) == 0
+    print 'os_07 ok'
+
+def os_08():
+    # TBD: setuid
+    os.getlogin() == 'gulan'
+    pwd.getpwuid(os.getuid()) == 'gulan'
+    # Note: os.environ is an dict-like instance of _Environ.
+    os.environ['USER'] == 'gulan'
+    os.environ['LOGNAME'] == 'gulan'
+
+    print 'os_08 ok'
+
+def os_09():
+    os.name == 'posix' # 'nt', 'os2', 'ce', 'java', 'riscos'
+    print 'os_09 ok'
     
+def os_10():
+    # status = os.system('echo hello-os05 >/dev/null')
+    status = os.system('sleep 0.1')
+    assert os.WCOREDUMP(status) == False
+    assert os.WIFCONTINUED(status) == False
+    assert os.WIFSTOPPED(status) == False
+    assert os.WIFSIGNALED(status) == False
+    assert os.WIFEXITED(status) == True
+    assert os.WEXITSTATUS(status) == 0
+
+    status = os.system('exit 0')
+    assert os.WCOREDUMP(status) == False
+    assert os.WIFCONTINUED(status) == False
+    assert os.WIFSTOPPED(status) == False
+    assert os.WIFSIGNALED(status) == False
+    assert os.WIFEXITED(status) == True
+    assert os.WEXITSTATUS(status) == 0
+    
+    status = os.system('exit 1')
+    assert os.WCOREDUMP(status) == False
+    assert os.WIFCONTINUED(status) == False
+    assert os.WIFSTOPPED(status) == False
+    assert os.WIFSIGNALED(status) == False
+    assert os.WIFEXITED(status) == True
+    assert os.WEXITSTATUS(status) == 1
+    
+    os.WSTOPSIG    #  types.BuiltinMethodType)
+    os.WTERMSIG    #  types.BuiltinMethodType)
+    os.WCOREDUMP    #  types.BuiltinMethodType)
+        #  types.BuiltinMethodType)
+    print 'os_10 ok'
+
+def os_11():
+    # For my system and locale:
+    assert os.strerror(1) == 'Operation not permitted'
+    assert os.strerror(2) == 'No such file or directory'
+    assert os.strerror(3) == 'No such process'
+    print 'os_11 ok'
+
+def os_12():
+    m = os.umask(0)             # set
+    assert os.umask(m) == 0     # restore
+    assert os.umask(123) == m   # set
+    assert os.umask(m) == 123   # restore
+    print 'os_12 ok'
+
+def os_13():
+    (sysname, nodename, release, version, machine) = os.uname()
+    assert sysname == 'Linux'
+    # print nodename
+    # print release
+    # print version
+    assert machine == 'x86_64'
+    (m, mlist, addrs) = socket.gethostbyaddr(socket.gethostname())
+    assert m == nodename
+    print 'os_13 ok'
+
+def os_14():
+    fd = os.open('os_14', os.O_WRONLY | os.O_CREAT)
+    rec = '<000><001><002><003><004><005><006><007><008><009>'
+    m = len(rec)
+    n = os.write(fd, rec)
+    while m - n:
+        n += os.write(fd, rec[n:])
+    os.close(fd)
+    
+    fd = os.open('os_14', os.O_RDONLY)
+    data = ''
+    r = os.read(fd, 11)
+    while r != '':
+        data += r
+        r = os.read(fd, 11)
+        
+    os.close(fd)
+    os.unlink('os_14')
+    assert data == rec
+    print 'os_14 ok'
+
+def os_15():
+    fd = os.open('os_15', os.O_WRONLY | os.O_CREAT)
+    fh = os.fdopen(fd, 'w')
+    rec = '<000><001><002><003><004><005><006><007><008><009>'
+    fh.write(rec)
+    fh.close()
+    try:                     # The file is completely closed.
+        os.write(fd, rec)
+    except OSError, e:
+        assert e.errno == 9  # Bad file descriptor
+    else:
+        assert 0
+    os.unlink('os_15')
+    print 'os_15 ok'
+
+def os_16():
+    rec = '<000><001><002><003><004><005><006><007><008><009>'
+    assert len(rec) == 50
+    
+    def write_data(fname):
+        fd = os.open(fname, os.O_WRONLY | os.O_CREAT)
+        m = len(rec)
+        n = os.write(fd, rec)
+        while m - n:
+            n += os.write(fd, rec[n:])
+        os.close(fd)
+    
+    write_data('os_16')
+    # Open the same file twice.
+    fd1 = os.open('os_16', os.O_RDONLY)
+    fd2 = os.open('os_16', os.O_RDONLY)
+    data1 = os.read(fd1, 25)
+    data2 = os.read(fd2, 25)        
+    os.close(fd2)
+    os.close(fd1)
+    os.unlink('os_16')
+    assert data1 == rec[:25]
+    assert data2 == rec[:25]  # offset not shared
+    print 'os_16 ok'
+
+def os_17():
+    rec = '<000><001><002><003><004><005><006><007><008><009>'
+    assert len(rec) == 50
+    
+    def write_data(fname):
+        fd = os.open(fname, os.O_WRONLY | os.O_CREAT)
+        m = len(rec)
+        n = os.write(fd, rec)
+        while m - n:
+            n += os.write(fd, rec[n:])
+        os.close(fd)
+    
+    write_data('os_17')
+    # Dup the opened file
+    fd1 = os.open('os_17', os.O_RDONLY)
+    fd2 = os.dup(fd1)
+    data1 = os.read(fd1, 25)
+    data2 = os.read(fd2, 25)        
+    os.close(fd2)
+    os.close(fd1)
+    os.unlink('os_17')
+    assert data1 == rec[:25]
+    assert data2 == rec[25:]  # offset *is* shared
+    print 'os_17 ok'
+
+def os_18():
+    rec = '<000><001><002><003><004><005><006><007><008><009>'
+    assert len(rec) == 50
+    
+    def write_data(fname):
+        fd = os.open(fname, os.O_WRONLY | os.O_CREAT)
+        m = len(rec)
+        n = os.write(fd, rec)
+        while m - n > 0:
+            n += os.write(fd, rec[n:])
+        os.close(fd)
+    
+    write_data('os_18')
+    fd = os.open('os_18', os.O_RDONLY)
+    assert os.SEEK_SET == 0
+    assert os.SEEK_CUR == 1
+    assert os.SEEK_END == 2
+    # Advance pos to 25.
+    assert os.lseek(fd, 25, os.SEEK_SET) == 25
+    # Now there are 25 more bytes to read.
+    data = os.read(fd, 25)
+    os.close(fd)
+    os.unlink('os_18')
+    assert data == rec[25:]
+    print 'os_18 ok'
+
+def os_19():
+    rec = '<000><001><002><003><004><005><006><007><008><009>'
+    assert len(rec) == 50
+    fname = 'os_19'
+    
+    fd = os.open(fname, os.O_WRONLY | os.O_CREAT)
+    m = len(rec)
+    n = os.write(fd, rec)
+    while m - n > 0:
+        n += os.write(fd, rec[n:])
+    os.ftruncate(fd, 40) # Drop the last 10 bytes
+    os.close(fd)
+    
+    fd = os.open('os_19', os.O_RDONLY)
+    data = os.read(fd, 100)
+    os.close(fd)
+    os.unlink('os_19')
+    assert data == rec[:40]
+
+    # ftruncate can also extend the file with holes, but it does not
+    # change the read/write position.
+    
+    N = 10
+    fd = os.open(fname, os.O_WRONLY | os.O_CREAT)
+    os.ftruncate(fd, N) # This is pointless because of the lseek
+    os.lseek(fd, N, 0)
+    m = len(rec)
+    n = os.write(fd, rec)
+    while m - n > 0:
+        n += os.write(fd, rec[n:])
+    os.close(fd)
+    
+    fd = os.open('os_19', os.O_RDONLY)
+    data = os.read(fd, max(N*2,100))
+    os.close(fd)
+    os.unlink('os_19')
+    
+    assert len(data) == len(rec) + N
+    assert data[:N] == '\000' * N
+    assert data[N:] == rec
+    
+    print 'os_19 ok'
+
+def os_20(): print 'os_20 ok'
+def os_21(): print 'os_21 ok'
+def os_22(): print 'os_22 ok'
+def os_23(): print 'os_23 ok'
+def os_24(): print 'os_24 ok'
+def os_25(): print 'os_25 ok'
+def os_26(): print 'os_26 ok'
+def os_27(): print 'os_27 ok'
+def os_28(): print 'os_28 ok'
+def os_29(): print 'os_29 ok'
+
 
 if __name__ == '__main__':
     os_01()
     os_02()
+    os_03()
+    os_04()
+    os_05()
+    os_06()
+    os_07()
+    os_08()
+    os_09()
+    os_10()
+    os_11()
+    os_12()
+    os_13()
+    os_14()
+    os_15()
+    os_16()
+    os_17()
+    os_18()
+    os_19()
